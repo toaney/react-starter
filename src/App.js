1cbcefa116bd9835c28d03930e1be24562712047
thomas toan <toaney@gmail.com>
2020-08-15 11:46:07 -0700

    wire up example input to use redux store

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import { updateUserInput } from './actions/userInputAction';
 
-function App() {
+const ReduxStateExample = ({dispatch, userInput}) => {
+  // set up local state with useState hook
+  // const [ userInput, setUserInput ] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React Starter
-        </a>
-      </header>
-    </div>
-  );
+      <div className="content-container">
+          <div className="content">
+              <div className="content-centered content-container">
+                  <h4>Quick example using redux to create a centralized source of truth</h4>
+                  <p>This component dispatches actions to update the userInput value in the redux store. React-redux's connect method and the mapStateToProps function defined in this component are used to read from the redux store. Input text below and see the "User Input" value update.</p>
+                  <p>Input Text: <input className="input-field" onChange={e => dispatch(updateUserInput(e.target.value))} value={userInput}/></p>
+                  <p>User Input: {userInput}</p>
+              </div>
+          </div>
+      </div>
+  )
 }
 
-export default App;
+//map redux state to component
+const mapStateToProps = state => {
+  return({
+      userInput: state.userInputReducer.userInput
+  })
+}
+
+export default connect(mapStateToProps)(ReduxStateExample);
